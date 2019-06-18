08996b8295c6b589714ca4e449c93566dab256a8
Q. Price <priceqp@gmail.com>
2019-06-18 17:01:41 -0500

    Move state and defaultProps to parent component

--- src/Box.js
+++ src/Box.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { choice, colorsArr } from './Helpers';
 import './Box.css';
 
 class Box extends React.Component {
-    static defaultProps = {
-        colors: colorsArr
-    }
     render() {
         return(
-            <div className='Box' style={{ backgroundColor: choice(this.props.colors) }} ></div>
+            <div className='Box' style={{ backgroundColor: this.props.color }} ></div>
         )
     }
 }
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import './BoxContainer.css';
 import Box from './Box';
+import { choice, colorsArr } from './Helpers';
+import './BoxContainer.css';
 
 class BoxContainer extends React.Component {
+    static defaultProps = {
+        colors: colorsArr
+    }
     constructor(props) {
         super(props);
-        this.state = {
-            numBoxes: 18
-        };
+        this.state = {numBoxes: 18};
+        this.randomColor = this.randomColor.bind(this);
     }
-
+    randomColor = () => choice(this.props.colors);
     render() {
-        const boxes = Array.from({length: this.state.numBoxes}).map(() => <Box />);
-        return(
-            <div className='BoxContainer'>
-                {boxes}
-            </div>
-        )
+        const boxes = Array.from({length: this.state.numBoxes}).map(() => <Box color={ this.randomColor() } />);
+        return(<div className='BoxContainer'>{boxes}</div>)
     }
 }
 
