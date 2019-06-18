c6dddf54a9624ecb8510236fcca8b0a8a278e780
Q. Price <priceqp@gmail.com>
2019-06-18 15:32:41 -0500

    Add colors array and random color generation

--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { choice, colorsArr } from './Helpers';
 import './Box.css';
 
 class Box extends React.Component {
+    static defaultProps = {
+        colors: colorsArr
+    }
     render() {
         return(
-            <div className='Box' style={{ backgroundColor: 'blue'}} ></div>
+            <div className='Box' style={{ backgroundColor: choice(this.props.colors) }} ></div>
         )
     }
 }
