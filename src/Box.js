3b0e9a3e5243acd0656b708bcd26897a52d630aa
Q. Price <priceqp@gmail.com>
2019-06-18 17:38:03 -0500

    Refactor code, work on click method

--- src/Box.js
+++ src/Box.js
@@ -4,7 +4,7 @@ import './Box.css';
 class Box extends React.Component {
     render() {
         return(
-            <div className='Box' style={{ backgroundColor: this.props.color }} ></div>
+            <div className='Box' style={{ backgroundColor: this.props.startColor }} ></div>
         )
     }
 }
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -5,16 +5,16 @@ import './BoxContainer.css';
 
 class BoxContainer extends React.Component {
     static defaultProps = {
-        colors: colorsArr
-    }
+        allColors: colorsArr,
+        numBoxes: 18
+    };
     constructor(props) {
         super(props);
-        this.state = {numBoxes: 18};
         this.randomColor = this.randomColor.bind(this);
     }
-    randomColor = () => choice(this.props.colors);
+    randomColor = () => choice(this.props.allColors);
     render() {
-        const boxes = Array.from({length: this.state.numBoxes}).map(() => <Box color={ this.randomColor() } />);
+        const boxes = Array.from({length: this.props.numBoxes}).map(() => <Box startColor={ this.randomColor() } />);
         return(<div className='BoxContainer'>{boxes}</div>)
     }
 }
